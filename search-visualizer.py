b6e408eb2d3eb11dd2a228e61f57442bcaccab86
Iain Mcilveen <45949570+IainMcilveen@users.noreply.github.com>
2020-05-26 20:52:04 -0300

    getting and return keyboard and mouse input

--- search-visualizer.py
+++ search-visualizer.py
@@ -9,13 +9,45 @@ import time
 win_width = 640
 win_height = 480
 
-def main():
+#initialize pygame window
+def initialize():
     #initialize pygame
     pygame.init()
 
     #create a window
-    win = pygame.display.set_mode((win_width,win_height))
+    window = pygame.display.set_mode((win_width,win_height))
     pygame.display.set_caption("Search Visualizer")
+
+    return window
+
+#get user inputs
+def inputs():
+
+    #get mouse inputs and position
+    mousePressed = pygame.mouse.get_pressed()
+    mousePos = pygame.mouse.get_pos()
+
+    #add all of the mouse inputs into a dictionary
+    mouse = {}
+    mouse["lmb"] = mousePressed[0]
+    mouse["rmb"] = mousePressed[2]
+    mouse["xPos"] = mousePos[0]
+    mouse["yPos"] = mousePos[1]
+
+    #get all keyboard inputs
+    keyInputs = pygame.key.get_pressed()
+
+    #storing needed keys into a dictionary
+    keys = {}
+    keys["esc"] = keyInputs[27]
+    keys["s"] = keyInputs[115]
+    keys["e"] = keyInputs[101]
+    
+    return mouse, keys
+
+def main():
+    
+    win = initialize()
 
     #main loop
     while True:
@@ -25,8 +57,10 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-                
-        
+
+        #get mouse and relevent key inputs        
+        mouse, keys = inputs()
+
         #draw a white screen
         win.fill(Color(255, 255, 255))
 
@@ -34,7 +68,6 @@ def main():
         pygame.display.update()
         time.sleep(0.1)
 
-        #close_win = True
 
 if __name__ == "__main__":
     main()
